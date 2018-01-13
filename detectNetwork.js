44d626f28220ea1b5249359140ebfe4f3fee4077
stevenkeezer <stevengkeezer@gmail.com>
2018-01-12 22:12:21 -0800

    this is my first commit

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,17 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+   
 };
 
 
+ /// here is some code to update my 
+ ///
+ //
+ /// HOPEFULLY NOTHIONG BREAKS
+ //
+ //
+ //
+
+ //
